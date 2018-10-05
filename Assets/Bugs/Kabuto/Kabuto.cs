51bd13211b67c22301c25ce75d7ce0888c078029
Speedwagon13 <kperras3@gatech.edu>
2018-10-05 14:07:16 -0400

    Kabuto Added
    Added Kabuto and made a slight change to CameraMain.cs. Also added a
    folder for bugs.

---
+++ Assets/Bugs/Kabuto/Kabuto.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kabuto : Bug {
+
+    private Animator anim;
+    // Use this for initialization
+    new void Start()
+    {
+        base.Start();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    new void Update()
+    {
+        base.Update();
+    }
+
+    public new void TriggerInteractable()
+    {
+        //idk how this is rly gonna work yet
+        base.TriggerInteractable();
+        anim.SetTrigger("trigger");
+        Debug.Log("KABUTO TRIGGERED");
+    }
+}
